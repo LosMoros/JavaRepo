bfc06a96e6c4206349fcc7e7ce7edec3d23e13f8
LosMoros <153547568@qq.com>
2018-06-30 19:23:48 +0800

    Using method to implement the judegment
    Using method to implement the judegment for JUnit test

--- minitest/src/main/java/minitest/FizzBuzz.java
+++ minitest/src/main/java/minitest/FizzBuzz.java
@@ -2,77 +2,91 @@ package minitest;
 
 public class FizzBuzz {
 
-	/*
-	 * print number from 1 to 100, 
-	 * if the number can be divisible by 3, print Fizz 
-	 * if the number can be divisible by 5, print Buzz 
-	 */	
-	public void myFizzBuzz1()  {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int i = 0;
-		boolean isDivBy3or5 = false;
+		
+		String printContent = "";
+		FizzBuzz myFizzBuzz = new FizzBuzz();
 
-		//loop from 1 to 100
+		// Case 1:loop from 1 to 100
 		for (i = 1; i <= 100; i++) {
-			isDivBy3or5 = false;
-
-			//if the number can be divisible by 3, print Fizz 
-			if (i % 3 == 0) {
-				System.out.print("Fizz");
-				isDivBy3or5 = true;
-			}
-
-			//if the number can be divisible by 5, print Buzz
-			if (i % 5 == 0) {
-				isDivBy3or5 = true;
-				System.out.print("Buzz");
-			}
-
-			//println normal number or switch to next line 
-			if (isDivBy3or5) {
-				System.out.println("");
-			} else {
-				System.out.println(i);
-			}
+			// Call myFizzBuzz1 to judge if the number can be divisible by 3 or 5
+			printContent = myFizzBuzz.myFizzBuzz1(i);
+			System.out.println(printContent);
 
 		}
 		
-	}
-	
-	
-	/*
-	 * print number from 1 to 100, 
-	 * if the number can be divisible by 3 or includes digit 3, print Fizz 
-	 * if the number can be divisible by 5 or includes digit 5, print Buzz 
-	 */	
-	public void myFizzBuzz2() {
-		int i = 0;
-		String digits = "";
-		boolean isDivBy3or5 = false;
-
-		//loop from 1 to 100
+		// Case 2: loop from 1 to 100
 		for (i = 1; i <= 100; i++) {
-			digits = String.valueOf(i);
-			isDivBy3or5 = false;
-
-			//if the number can be divisible by 3 or have 3 in it, print Fizz 
-			if (i % 3 == 0 || digits.indexOf("3") != -1) {
-				isDivBy3or5 = true;
-				System.out.print("Fizz");
-			}
-
-			//if the number can be divisible by 5 or have 5 in it, print Buzz
-			if (i % 5 == 0 || digits.indexOf("5") != -1) {
-				isDivBy3or5 = true;
-				System.out.print("Buzz");
-			}
-			
-			//println normal number or switch to next line 
-			if (isDivBy3or5) {
-				System.out.println("");
-			} else {
-				System.out.println(i);
-			}
+			// Call myFizzBuzz2 to judge if the number can be divisible by 3 or 5, and have digit 3 or 5
+			printContent = myFizzBuzz.myFizzBuzz2(i);
+			System.out.println(printContent);
 
 		}
+	}
+
+	/*
+	 * This method returns string "Fizz" if the input number can be divisible by 3, 
+	 * return "Buzz" if the number can be divisible by 5.
+	 * 
+	 * @param number The number to be judged
+	 * @return printResult returns input nubmer or Fizz Buzz.
+	 */
+	public String myFizzBuzz1(int number) {
+		boolean isDivBy3or5 = false;
+		String printResult = "";
+
+		// if the number can be divisible by 3, print Fizz
+		if (number % 3 == 0) {
+			isDivBy3or5 = true;
+			printResult = "Fizz";
+		}
+
+		// if the number can be divisible by 5, print Buzz
+		if (number % 5 == 0) {
+			isDivBy3or5 = true;
+			printResult = printResult + "Buzz";
+		}
+		
+		if (!isDivBy3or5) {
+			printResult = String.valueOf(number);
+		}
+
+		return printResult;
+
+	}
+
+	/*
+	 * This method returns string "Fizz" if the input number can be divisible by 3 or includes
+	 * digit 3, return "Buzz" if the number can be divisible by 5 or includes digit 5.
+	 * 
+	 * @param number The number to be judged
+	 * @return printResult returns input nubmer or Fizz Buzz.
+	 */
+	public String myFizzBuzz2(int number) {
+		boolean isDivBy3or5 = false;
+		String printResult = "";
+		String digits = String.valueOf(number);
+
+		// if the number can be divisible by 3 or have 3 in it, print Fizz
+		if (number % 3 == 0 || digits.indexOf("3") != -1) {
+			isDivBy3or5 = true;
+			printResult = "Fizz";
+		}
+
+		// if the number can be divisible by 5 or have 5 in it, print Buzz
+		if (number % 5 == 0 || digits.indexOf("5") != -1) {
+			isDivBy3or5 = true;
+			printResult = printResult + "Buzz";
+		}
+
+		if (!isDivBy3or5) {
+			printResult = String.valueOf(number);
+		}
+
+		return printResult;
+		
+		
 	}
 }
--- minitest/src/test/java/minitest/TestFizzBuzz.java
+++ minitest/src/test/java/minitest/TestFizzBuzz.java
@@ -1,29 +1,77 @@
 package minitest;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class TestFizzBuzz {
 	/*
-	 * print number from 1 to 100, 
+	 * test function myFizzBuzz1 
 	 * if the number can be divisible by 3, print Fizz 
 	 * if the number can be divisible by 5, print Buzz 
 	 */
 	@Test
 	public void testCase1()  {
 		FizzBuzz myFizzBuzz1 = new FizzBuzz();
-		myFizzBuzz1.myFizzBuzz1();
+		System.out.println(myFizzBuzz1.myFizzBuzz1(3));
+		assertEquals(myFizzBuzz1.myFizzBuzz1(3),"Fizz");
+		
+		System.out.println(myFizzBuzz1.myFizzBuzz1(5));
+		assertEquals(myFizzBuzz1.myFizzBuzz1(5),"Buzz");
+		
+		System.out.println(myFizzBuzz1.myFizzBuzz1(15));
+		assertEquals(myFizzBuzz1.myFizzBuzz1(15),"FizzBuzz");
+		
+		System.out.println(myFizzBuzz1.myFizzBuzz1(1));
+		assertEquals(myFizzBuzz1.myFizzBuzz1(1),"1");
+		
+		System.out.println(myFizzBuzz1.myFizzBuzz1(-1));
+		assertEquals(myFizzBuzz1.myFizzBuzz1(-1),"-1");
+		
+		System.out.println(myFizzBuzz1.myFizzBuzz1(0));
+		assertEquals(myFizzBuzz1.myFizzBuzz1(0),"FizzBuzz");
+		
 		
 	}
 	
 	
 	/*
-	 * print number from 1 to 100, 
+	 * test function myFizzBuzz2 
 	 * if the number can be divisible by 3 or includes digit 3, print Fizz 
 	 * if the number can be divisible by 5 or includes digit 5, print Buzz 
 	 */
-	@Test
+	@Test	
 	public void testCase2() {
 		FizzBuzz myFizzBuzz2 = new FizzBuzz();
-		myFizzBuzz2.myFizzBuzz2();
+		System.out.println(myFizzBuzz2.myFizzBuzz2(3));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(3),"Fizz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(5));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(5),"Buzz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(15));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(15),"FizzBuzz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(31));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(31),"Fizz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(35));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(35),"FizzBuzz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(52));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(52),"Buzz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(54));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(54),"FizzBuzz");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(1));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(1),"1");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(-1));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(-1),"-1");
+		
+		System.out.println(myFizzBuzz2.myFizzBuzz2(0));
+		assertEquals(myFizzBuzz2.myFizzBuzz2(0),"FizzBuzz");	
+		
 	}
 }
